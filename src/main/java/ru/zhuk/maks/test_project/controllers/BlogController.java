4c3e232956ea711585f9765a3abd689a5920d872
Maksim Zhuk <maksimzhu1994@yandex.ru>
2021-08-31 22:48:12 +0300

    Second commit to master

---
+++ src/main/java/ru/zhuk/maks/test_project/controllers/BlogController.java
@@ -0,0 +1,13 @@
+package ru.zhuk.maks.test_project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/blog")
+    public String blog(Model model){
+        return "blog-main";
+    }
+}
