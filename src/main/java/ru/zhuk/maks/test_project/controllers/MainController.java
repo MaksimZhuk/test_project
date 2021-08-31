5494aec95ab24d688a37a1f9f854ccd9be23bf9c
Maksim Zhuk <maksimzhu1994@yandex.ru>
2021-08-31 21:34:02 +0300

    First commit to master

---
+++ src/main/java/ru/zhuk/maks/test_project/controllers/MainController.java
@@ -0,0 +1,17 @@
+package ru.zhuk.maks.test_project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+}
---
+++ src/test/java/ru/zhuk/maks/test_project/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhuk.maks.test_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
