878ccc11cf7a36f3d020856417ed147580bc1d1b
Maksim Zhuk <maksimzhu1994@yandex.ru>
2021-09-04 18:50:56 +0300

    Buttons to edit/delete

--- src/main/java/ru/zhuk/maks/test_project/controllers/BlogController.java
+++ src/main/java/ru/zhuk/maks/test_project/controllers/BlogController.java
@@ -40,11 +40,49 @@ public class BlogController {
 
     @GetMapping("/blog/{id_num}")
     public String blogDetails(@PathVariable(value="id_num") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
         Optional<Post> post = postRepository.findById(id);
-        ArrayList<Post> res = new ArrayList<>();
-        post.ifPresent(res::add);
+//        ArrayList<Post> res = new ArrayList<>();
+//        post.ifPresent(res::add);
         model.addAttribute("post", post.get());
         return "blog-details";
+    }
+    @GetMapping("/blog/{id_num}/edit")
+    public String blogEdit(@PathVariable(value="id_num") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+//        ArrayList<Post> res = new ArrayList<>();
+//        post.ifPresent(res::add);
+        model.addAttribute("post", post.get());
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id_num}/edit")
+    public String blogUpdate(@PathVariable(value="id_num") Long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> opt_post = postRepository.findById(id);
+        Post post = opt_post.get();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id_num}/remove")
+    public String blogRemove(@PathVariable(value="id_num") Long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
     }
 
 }
